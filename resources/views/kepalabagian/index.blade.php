1de1e7be895cfe22aa2711d37728577f63060ac6
Marshallramdh <marshall.120140017@student.itera.ac.id>
2024-01-12 00:35:10 +0700

    Dashboard kabag dan admin

--- resources/views/kepalabagian/index.blade.php
+++ resources/views/kepalabagian/index.blade.php
@@ -1,131 +1,156 @@
 @extends('layouts.appdashboardkabag')
 
 @section('content')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css">
     <link rel="stylesheet" href="css/home.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <style>
+      canvas {
+        max-width: 100%;
+        margin: 20px 0;
+      }
+    </style>
+
 
     <div class="container">
         <div class="row">
-            <div class="col-lg-3 col-12">
+            <div class="col-lg-3 col-6">
                 <div class="small-box">
-                    <div class="inner" style="text-align: center">
+                    <div class="inner">
                         <h3>{{ $countUsers }}</h3>
-                        <p>Pengguna</p>
+                        <p>Karyawan</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-bag"></i>
                     </div>
-                    <a href="{{ route('KBprofile.user') }}" class="small-box-footer">Jumlah Pengguna <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ route('user.index') }}" class="small-box-footer">Jumlah Karyawan<i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
-    
-            <div class="col-lg-3 col-12">
+
+            <div class="col-lg-3 col-6">
                 <div class="small-box">
-                    <div class="inner" style="text-align: center">
+                    <div class="inner">
+                        <h3>{{ $countArsips }}</h3>
+                        <p>Cuti</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="{{ route('arsip.index') }}" class="small-box-footer">Jumlah Cuti <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-6">
+                <div class="small-box">
+                    <div class="inner">
                         <h3>{{ $countSuratMasuk }}</h3>
-                        <p>Surat Masuk</p>
+                        <p>Izin</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-person-add"></i>
                     </div>
-                    <a href="{{ route('kbsuratmasuk.index') }}" class="small-box-footer">Jumlah Surat Masuk <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ route('suratkeluar.index') }}" class="small-box-footer">Jumlah Izin <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
-    
-            <div class="col-lg-3 col-12">
+
+            <div class="col-lg-3 col-6">
                 <div class="small-box">
-                    <div class="inner" style="text-align: center">
+                    <div class="inner">
                         <h3>{{ $countSuratKeluar }}</h3>
-                        <p>Surat Keluar</p>
+                        <p>Tukar Jaga</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-pie-graph"></i>
                     </div>
-                    <a href="{{ route('kbsuratkeluar.index') }}" class="small-box-footer">Jumlah Surat Keluar <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ route('suratmasuk.index') }}" class="small-box-footer">Jumlah Tukar Jaga <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
         </div>
-    </div>
-    
 
-        <!-- Sisipkan script untuk Chart.js (jika belum ada) -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
-        {{-- <script>
-            // Mengambil data dari database (contoh: menggunakan format JSON)
-            var suratData = {!! json_encode($suratData) !!};
-             var suratDataJson = {!! $suratDataJson !!};
+        {{-- {{-- <div class="container"> --}}
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header" style="background-color: #0051B9;color:white">
+                            <h4 class="card-title">Telat Hari Ini</h4>
+                        </div>
+                        <div class="card-body">
+                            <table class="table" >
+                                <thead style="background-color: #0051B9;color:white"">
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama</th>
+                                        <th>Bagian</th>
+                                        <th>Shift</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <td>1</td>
+                                            <td>Agus</td>
+                                            <td>Dokter</td>
+                                            <td>Pagi</td>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="card-footer">
+                            <button class="btn" style="background-color: rgb(33, 0, 219);color:aliceblue;">&#8594;</button>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header" style="background-color: #0051B9;color:white">
+                            <h4 class="card-title">Karyawan Cuti/Izin</h4>
+                        </div>
+                        <div style="text-align: center;">
+                            <!-- Tambahkan elemen canvas sebagai tempat diagram batang -->
+                            <canvas id="barChart"></canvas>
+                          </div>
+                        
+                          <script>
+                            // Data untuk diagram batang
+                            var data = {
+                              labels: ["Cuti", "Izin"],
+                              datasets: [{
+                                label: 'Karyawan',
+                                data: [10, 20], // Tinggi untuk setiap kategori
+                                backgroundColor: [
+                                  'rgba(255, 99, 132, 0.7)',
+                                  'rgba(54, 162, 235, 0.7)',
+                                ],
+                                borderColor: [
+                                  'rgba(255, 99, 132, 1)',
+                                  'rgba(54, 162, 235, 1)',
+                                ],
+                                borderWidth: 1
+                              }]
+                            };
+                        
+                            // Konfigurasi untuk diagram batang
+                            var options = {
+                              scales: {
+                                y: {
+                                  beginAtZero: true
+                                }
+                              }
+                            };
+                        
+                            // Mengambil elemen canvas
+                            var ctx = document.getElementById('barChart').getContext('2d');
+                        
+                            // Membuat objek diagram batang
+                            var barChart = new Chart(ctx, {
+                              type: 'bar',
+                              data: data,
+                              options: options
+                            });
+                          </script>
+                    </div>
+                </div>
+            </div>
+        </div> 
 
-            // alert("The Data: " + suratDataJson);
 
-            // Jika suratData bukan array, Anda perlu menyesuaikan langkah-langkah berikutnya
-            if (!Array.isArray(suratData)) {
-                console.error("Format data tidak sesuai. Pastikan data yang diterima dari server berupa array.");
-            }
-
-            // Lanjutkan dengan memeriksa struktur data dan melakukan langkah-langkah sesuai
-            // ...
-
-            // Membuat array untuk label bulan
-            var monthLabels = suratData.map(function (data) {
-                return data.month;
-            });
-
-            // Membuat array untuk data surat masuk
-            var suratMasukData = suratData.map(function (data) {
-                return data.surat_masuk;
-            });
-
-            // Membuat array untuk data surat keluar
-            var suratKeluarData = suratData.map(function (data) {
-                return data.surat_keluar;
-            });
-
-            // Data yang akan ditampilkan pada grafik
-            var data = {
-                labels: monthLabels,
-                datasets: [{
-                    label: 'Surat Masuk',
-                    backgroundColor: 'rgba(0, 123, 255, 0.7)', // Warna latar belakang
-                    borderColor: 'rgba(0, 123, 255, 1)',       // Warna garis tepi
-                    borderWidth: 1,
-                    data: suratMasukData
-                },
-                {
-                    label: 'Surat Keluar',
-                    backgroundColor: 'rgba(255, 193, 7, 0.7)',
-                    borderColor: 'rgba(255, 193, 7, 1)',
-                    borderWidth: 1,
-                    data: suratKeluarData
-                }]
-            };
-
-            // Konfigurasi grafik
-            var options = {
-                responsive: true,
-                maintainAspectRatio: false,
-                scales: {
-                    xAxes: [{
-                        barPercentage: 0.8,
-                    }],
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true,
-                            stepSize: 25
-                        }
-                    }]
-                }
-            };
-
-            // Membuat grafik
-            var ctx = document.getElementById('barChart').getContext('2d');
-            var myBarChart = new Chart(ctx, {
-                type: 'bar',
-                data: data,
-                options: options
-            });
-        </script> --}}
-
-
-
+        
     </div>
 @endsection
